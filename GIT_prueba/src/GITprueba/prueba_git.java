08209bd44e9f4cd184a1793e4dec900488f53e1f
Diurno <Diurno@pc0114>
2023-05-05 13:08:39 +0200

    primer commit

---
+++ GIT_prueba/src/GITprueba/prueba_git.java
@@ -0,0 +1,9 @@
+package GITprueba;
+
+public class prueba_git {
+
+	public static void main(String[] args) {
+		System.out.println("control prueba GIT");
+	}
+
+}
---
+++ GIT_prueba/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Diurno
+ *
+ */
+module GIT_prueba {
+}
